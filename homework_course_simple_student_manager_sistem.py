1df966f3d345aabf61a6825911d327360c159f14
Taki <takicakb@gmail.com>
2020-12-26 19:47:49 +0300

    Update homework_course_simple_student_manager_sistem.py

--- homework_course_simple_student_manager_sistem.py
+++ homework_course_simple_student_manager_sistem.py
@@ -7,6 +7,19 @@ Created on Sat Dec 26 04:08:45 2020
 import sys
 import random
 
+def display_grade_str(score):
+    if score >= 90:
+        return("AA")
+    elif 70 <= score < 90:
+        return("BB")
+    elif 50 <= score < 70:
+        return("CC")
+    elif 30 <= score < 50:
+        return("DD")
+    else:
+        return("FF You Fail")
+    
+
 #login olabilecek user listesi
 userlist = [ ["ömer", "cengiz"], ["elif", "yiğit"], ["kutay", "akalın"], ["aysuda","ceylan"] ]
 
@@ -57,7 +70,7 @@ for lesson in lessons:
     
 print("Please Select min 3, max 5 lesson for Yourself.")
 
-selectedlessonscount = 1
+selectedlessonscount = 0
 selectedlessons = []
 selectedlesson = -1
 
@@ -71,8 +84,14 @@ while not (selectedlesson == 0 and 3 <= selectedlessonscount <= 5 ):
     
     if selectedlesson in (1,2,3,4,5) and lessons[selectedlesson-1] in lessons:
         if lessons[selectedlesson-1] not in selectedlessons: 
-            selectedlessons.append(selectedlesson)
-            selectedlesson += 1
+            selectedlessons.append(lessons[selectedlesson-1])
+            selectedlessonscount += 1
+            print("Selected Lessons ("+str(selectedlessonscount)+") : ", end="")
+            for seles in selectedlessons:
+                print(seles, ", ", end="")
+        else:
+            print("You Select This Lesson Before")
+                
     elif selectedlesson == 0:
         if not (3 <= selectedlessonscount <= 5):
             print("You Should Select min 3, max 5 lesson for Yourself.")
@@ -83,9 +102,14 @@ grades = {"midterm":random.randint(0, 100),
           "final":random.randint(0, 100),
           "project":random.randint(0, 100)}
 
-
+score = 0
+print()
+print("--------------------------------------------")
 print("Your", selectedlessons[0], "Lesson Exam Grades")
-print("MidTerm Exam: ", grades["midterm"], "Score Percent(30%) :")
-print("Final   Exam: ", grades["final"],   "Score Percent(50%) :")
-print("Project Exam: ", grades["project"], "Score Percent(20%) :")
-print("Total Score :")  ## AA-FF not dönen fonksiyon yazılacak
+score += grades["midterm"]*30/100
+score += grades["final"]*50/100
+score += grades["project"]*20/100
+print("MidTerm Exam: ", grades["midterm"], "Score Percent(30%) :", grades["midterm"]*30/100)
+print("Final   Exam: ", grades["final"],   "Score Percent(50%) :", grades["final"]*50/100)
+print("Project Exam: ", grades["project"], "Score Percent(20%) :", grades["project"]*20/100)
+print("Total Score :", score, "Note is", display_grade_str(score))  
